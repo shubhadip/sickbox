a74562751f7e8c8cd23c92d54ee05f199b3ec5da
shubhadip <shubhadip.maity@gmail.com>
2020-03-28 15:19:35 +0530

    changes for defer

--- webpack.dev.js
+++ webpack.dev.js
@@ -93,6 +93,7 @@ module.exports = {
     new htmlWebpackPlugin({
       template: path.resolve(__dirname, 'src/index.html'),
       filename: 'index.html',
+      scriptLoading: 'defer'
     }),
   ],
   optimization:{
